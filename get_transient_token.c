44b7b6d1af25c97e2b1059c9293bab222bfc9580
Ben Cohen <ben-cohen@users.noreply.github.com>
2017-06-25 14:56:11 +0100

    Reindent source code

--- get_transient_token.c
+++ get_transient_token.c
@@ -40,168 +40,168 @@
 
 int getrandbase64(char *data_base64)
 {
-  int rc;
-  char data_raw[CHALLENGE_SIZE_RAW_BYTES];
-  BIO *mem = BIO_new(BIO_s_mem());
-  if (mem == NULL)
-    return -1;
+    int rc;
+    char data_raw[CHALLENGE_SIZE_RAW_BYTES];
+    BIO *mem = BIO_new(BIO_s_mem());
+    if (mem == NULL)
+        return -1;
 
-  BIO *base64 = BIO_new(BIO_f_base64());
-  if (base64 == NULL)
-    return -1;
+    BIO *base64 = BIO_new(BIO_f_base64());
+    if (base64 == NULL)
+        return -1;
 
-  mem = BIO_push(base64, mem);
-  if (mem == NULL)
-    return -1;
+    mem = BIO_push(base64, mem);
+    if (mem == NULL)
+        return -1;
 
-  rc = RAND_bytes(data_raw, sizeof(data_raw));
-  if (rc != 1)
-    return -1;
+    rc = RAND_bytes(data_raw, sizeof(data_raw));
+    if (rc != 1)
+        return -1;
 
-  rc = BIO_write(base64, data_raw, sizeof(data_raw));
-  if (rc != sizeof(data_raw))
-    return -1;
+    rc = BIO_write(base64, data_raw, sizeof(data_raw));
+    if (rc != sizeof(data_raw))
+        return -1;
 
-  BIO_flush(mem);
+    BIO_flush(mem);
 
-  char *bio_data_base64;
-  int len = BIO_get_mem_data(mem, &bio_data_base64);
-  if (len != CHALLENGE_SIZE_BASE64_BYTES + 1)
-    return -1;
-  memcpy(data_base64, bio_data_base64, CHALLENGE_SIZE_BASE64_BYTES);
+    char *bio_data_base64;
+    int len = BIO_get_mem_data(mem, &bio_data_base64);
+    if (len != CHALLENGE_SIZE_BASE64_BYTES + 1)
+        return -1;
+    memcpy(data_base64, bio_data_base64, CHALLENGE_SIZE_BASE64_BYTES);
 
-  BIO_free_all(mem);
+    BIO_free_all(mem);
 
-  return 0;
+    return 0;
 }
 
 
 int main()
 {
-  int rc;
+    int rc;
 
-  /* Create unix domain socket */
-  char udspath[MAX_UDS_PATH] = "/tmp/uds";
-  unlink(udspath);
-
-  int fd = socket(AF_UNIX, SOCK_STREAM, 0);
-  if (fd == -1)
-  {
-    fprintf(stderr, "socket creation failed\n");
-    exit(1);
-  }
-
-  struct sockaddr_un addr;
-  memset(&addr, 0, sizeof(addr));
-  addr.sun_family = AF_UNIX;
-  strncpy(addr.sun_path, udspath, sizeof(addr.sun_path) - 1);
-  rc = bind(fd, (struct sockaddr*)&addr, sizeof(addr));
-  if (rc != 0)
-  {
-    fprintf(stderr, "bind failed\n");
-    close(fd);
-    exit(1);
-  }
-  
-  rc = chmod(udspath, 0600);
-  if (rc != 0)
-  {
-    fprintf(stderr, "chmod failed\n");
-    close(fd);
+    /* Create unix domain socket */
+    char udspath[MAX_UDS_PATH] = "/tmp/uds";
     unlink(udspath);
-    exit(1);
-  }
 
-  rc = listen(fd, 1);
-  if (rc != 0)
-  {
-    fprintf(stderr, "listen failed\n");
-    close(fd);
-    unlink(udspath);
-    exit(1);
-  }
-
-  /* Generate a random challenge and response */
-  char challenge[CHALLENGE_SIZE_BASE64_BYTES];
-  char response[CHALLENGE_SIZE_BASE64_BYTES];
-  rc = getrandbase64(challenge);
-  if (rc != 0)
-  {
-    fprintf(stderr, "failed to generate challenge\n");
-    close(fd);
-    unlink(udspath);
-    exit(1);
-  }
-  rc = getrandbase64(response);
-  if (rc != 0)
-  {
-    fprintf(stderr, "failed to generate response\n");
-    close(fd);
-    unlink(udspath);
-    exit(1);
-  }
-
-  /* Generate and print the token */
-  char buffer[255];
-  int len;
-  len = snprintf(buffer, sizeof(buffer),
-                 "TRANSTOK:%d:%d:%s:%.*s:%.*s:",
-                 getuid(),
-                 getpid(),
-                 udspath,
-                 CHALLENGE_SIZE_BASE64_BYTES,
-                 challenge,
-                 CHALLENGE_SIZE_BASE64_BYTES,
-                 response);
-  if (len == sizeof(buffer))
-  {
-    fprintf(stderr, "Buffer too small\n");
-    close(fd);
-    unlink(udspath);
-    exit(1);
-  }
-
-  /* Print the token */
-  printf("%s\n", buffer);
-  memset(buffer, 0, sizeof(buffer));
-
-  /* Daemonize */
-  fflush(stdout);
-  daemon(0, 0);
-
-  /* Listen for up to the timeout */
-  // TODO: The timeout could be a command-line option
-  struct timeval timeout;
-  timeout.tv_sec = TIMEOUT_SECS;
-  timeout.tv_usec = 0;
-  fd_set readfds;
-  FD_ZERO(&readfds);
-  FD_SET(fd, &readfds);
-  select(FD_SETSIZE, &readfds, NULL, NULL, &timeout);
-  
-  /* If something connected then get it... */
-  if (FD_ISSET(fd, &readfds))
-  {
-    // TODO: This bit should also timeout
-    int addrsize = sizeof(addr);
-    int fdc = accept(fd, (struct sockaddr*)&addr, &addrsize);
-
-    char inchallenge[CHALLENGE_SIZE_BASE64_BYTES];  
-    read(fdc, inchallenge, CHALLENGE_SIZE_BASE64_BYTES);
-    if (memcmp(challenge, inchallenge, CHALLENGE_SIZE_BASE64_BYTES) == 0)
+    int fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (fd == -1)
     {
-      write(fdc, response, sizeof(response));
-      write(fdc, "\n", 1);
+        fprintf(stderr, "socket creation failed\n");
+        exit(1);
     }
-    else
-    {
-      write(fdc, "FAIL\n", 5);
-    }
-    close(fdc);
-  }
 
-  /* Tidy up */
-  close(fd);
-  unlink(udspath);
-  return 0;
+    struct sockaddr_un addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, udspath, sizeof(addr.sun_path) - 1);
+    rc = bind(fd, (struct sockaddr*)&addr, sizeof(addr));
+    if (rc != 0)
+    {
+        fprintf(stderr, "bind failed\n");
+        close(fd);
+        exit(1);
+    }
+
+    rc = chmod(udspath, 0600);
+    if (rc != 0)
+    {
+        fprintf(stderr, "chmod failed\n");
+        close(fd);
+        unlink(udspath);
+        exit(1);
+    }
+
+    rc = listen(fd, 1);
+    if (rc != 0)
+    {
+        fprintf(stderr, "listen failed\n");
+        close(fd);
+        unlink(udspath);
+        exit(1);
+    }
+
+    /* Generate a random challenge and response */
+    char challenge[CHALLENGE_SIZE_BASE64_BYTES];
+    char response[CHALLENGE_SIZE_BASE64_BYTES];
+    rc = getrandbase64(challenge);
+    if (rc != 0)
+    {
+        fprintf(stderr, "failed to generate challenge\n");
+        close(fd);
+        unlink(udspath);
+        exit(1);
+    }
+    rc = getrandbase64(response);
+    if (rc != 0)
+    {
+        fprintf(stderr, "failed to generate response\n");
+        close(fd);
+        unlink(udspath);
+        exit(1);
+    }
+
+    /* Generate and print the token */
+    char buffer[255];
+    int len;
+    len = snprintf(buffer, sizeof(buffer),
+                   "TRANSTOK:%d:%d:%s:%.*s:%.*s:",
+                   getuid(),
+                   getpid(),
+                   udspath,
+                   CHALLENGE_SIZE_BASE64_BYTES,
+                   challenge,
+                   CHALLENGE_SIZE_BASE64_BYTES,
+                   response);
+    if (len == sizeof(buffer))
+    {
+        fprintf(stderr, "Buffer too small\n");
+        close(fd);
+        unlink(udspath);
+        exit(1);
+    }
+
+    /* Print the token */
+    printf("%s\n", buffer);
+    memset(buffer, 0, sizeof(buffer));
+
+    /* Daemonize */
+    fflush(stdout);
+    daemon(0, 0);
+
+    /* Listen for up to the timeout */
+    // TODO: The timeout could be a command-line option
+    struct timeval timeout;
+    timeout.tv_sec = TIMEOUT_SECS;
+    timeout.tv_usec = 0;
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(fd, &readfds);
+    select(FD_SETSIZE, &readfds, NULL, NULL, &timeout);
+
+    /* If something connected then get it... */
+    if (FD_ISSET(fd, &readfds))
+    {
+        // TODO: This bit should also timeout
+        int addrsize = sizeof(addr);
+        int fdc = accept(fd, (struct sockaddr*)&addr, &addrsize);
+
+        char inchallenge[CHALLENGE_SIZE_BASE64_BYTES];  
+        read(fdc, inchallenge, CHALLENGE_SIZE_BASE64_BYTES);
+        if (memcmp(challenge, inchallenge, CHALLENGE_SIZE_BASE64_BYTES) == 0)
+        {
+            write(fdc, response, sizeof(response));
+            write(fdc, "\n", 1);
+        }
+        else
+        {
+            write(fdc, "FAIL\n", 5);
+        }
+        close(fdc);
+    }
+
+    /* Tidy up */
+    close(fd);
+    unlink(udspath);
+    return 0;
 }
--- pam_transient_token.c
+++ pam_transient_token.c
@@ -36,49 +36,49 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh,
                                    int argc,
                                    const char **argv)
 {
-   int rc;
-   const char *token;
+    int rc;
+    const char *token;
 
-   rc = pam_get_item(pamh, PAM_AUTHTOK, (const void **)&token);
-   if (rc != PAM_SUCCESS)
-     return PAM_AUTHINFO_UNAVAIL;
+    rc = pam_get_item(pamh, PAM_AUTHTOK, (const void **)&token);
+    if (rc != PAM_SUCCESS)
+        return PAM_AUTHINFO_UNAVAIL;
 
-   /*
-    * Parse the token.  It should look like:
-    *    TRANSTOK:<uid>:<pid>:<udspath>:<challenge>:<response>:"
-    */
-   char token_challenge[CHALLENGE_SIZE_BASE64_BYTES];
-   char token_response[CHALLENGE_SIZE_BASE64_BYTES];
-   char token_udspath[MAX_UDS_PATH];
-   int token_pid;
-   int token_uid;
-   if (sscanf(token, "TRANSTOK:%d:%d:%.*s:%.*s:%.*s:",
-              &token_uid,
-              &token_pid,
-              token_udspath,
-              token_challenge,
-              token_response) != 5)
-     return PAM_AUTH_ERR;
+    /*
+     * Parse the token.  It should look like:
+     *    TRANSTOK:<uid>:<pid>:<udspath>:<challenge>:<response>:"
+     */
+    char token_challenge[CHALLENGE_SIZE_BASE64_BYTES];
+    char token_response[CHALLENGE_SIZE_BASE64_BYTES];
+    char token_udspath[MAX_UDS_PATH];
+    int token_pid;
+    int token_uid;
+    if (sscanf(token, "TRANSTOK:%d:%d:%.*s:%.*s:%.*s:",
+               &token_uid,
+               &token_pid,
+               token_udspath,
+               token_challenge,
+               token_response) != 5)
+        return PAM_AUTH_ERR;
 
-   /* Find out which user is trying to log in. */
-   const char *user;
-   rc = pam_get_user(pamh, &user, NULL);
-   if (rc != PAM_SUCCESS)
-     return PAM_AUTHINFO_UNAVAIL;
+    /* Find out which user is trying to log in. */
+    const char *user;
+    rc = pam_get_user(pamh, &user, NULL);
+    if (rc != PAM_SUCCESS)
+        return PAM_AUTHINFO_UNAVAIL;
 
-   /* Verify that the user matches the uid from the token. */
-   struct passwd *p = getpwnam(user);
-   if (p == NULL)
-     return PAM_AUTHINFO_UNAVAIL;
-   if (p->pw_uid != uid)
-     return PAM_AUTH_ERR;
+    /* Verify that the user matches the uid from the token. */
+    struct passwd *p = getpwnam(user);
+    if (p == NULL)
+        return PAM_AUTHINFO_UNAVAIL;
+    if (p->pw_uid != uid)
+        return PAM_AUTH_ERR;
 
-   /* Open the unix domain socket given.  Check that it matches the given uid
-    * and pid. */
+    /* Open the unix domain socket given.  Check that it matches the given uid
+     * and pid. */
 
-   /* Send the challenge and check that the response is correct. */
+    /* Send the challenge and check that the response is correct. */
 
-   return PAM_SUCCESS;
+    return PAM_SUCCESS;
 }
 
 
@@ -87,20 +87,20 @@ PAM_EXTERN int pam_sm_setcred(pam_handle_t *pamh,
                               int argc,
                               const char **argv)
 {
-  return PAM_SUCCESS;
+    return PAM_SUCCESS;
 }
 
 
 #ifdef PAM_STATIC             /* for the case that this module is static */
 
 struct pam_module _pam_transient_token_modstruct = {   /* static module data */
-     "pam_transient_token",
-     pam_sm_authenticate,
-     pam_sm_setcred,
-     NULL,
-     NULL,
-     NULL,
-     pam_sm_chauthtok,
+    "pam_transient_token",
+    pam_sm_authenticate,
+    pam_sm_setcred,
+    NULL,
+    NULL,
+    NULL,
+    pam_sm_chauthtok,
 };
 
 #endif                                                 /* end PAM_STATIC */
